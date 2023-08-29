9e1e4b93cbc0541f23345cf0be1c3f598bcc9e82
Emeka Nwadiogbu, Jr <emekanwadiogbu.jr@me.com>
2023-08-29 17:47:26 -0600

    Created the ReactJS code for the Header and Header.css components

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React from "react";
 
-import './Header.css';
+import { images } from "../../constants";
+import "./Header.css";
+import { SubHeading } from "../../components";
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new flavor" />
+      <h1 className="app__header-h1">The Key to Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: "2rem 0" }}>
+        Enjoy a class A dining experience in the Biltmore Phoenix's Gericht
+        Restaurant. Lead Chef, Emeka Nwadiogbu, Jr., has won countless awards
+        for his organic, healthy seafood and salmon specialty entres. At
+        Gericht, we strive to provide uncomparable winery and dining that is
+        unmatched!
+      </p>
+      <button type="button" className="custom__button">
+        Explore Menu
+      </button>
+    </div>
+
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="header img" />
+    </div>
   </div>
 );
 
