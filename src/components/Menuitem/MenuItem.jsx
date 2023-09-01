1da57943a4b8f0934e46508ed51825815d00a0ad
Emeka Nwadiogbu, Jr <emekanwadiogbu.jr@me.com>
2023-09-01 02:25:17 -0600

    Completed the Menu with SpecialMenu along with Cocktail prices and items. Also completed the Chef section added image of chef with CSS styles sheet

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React from "react";
 
-import './MenuItem.css';
+import "./MenuItem.css";
 
-const MenuItem = () => (
-  <div>
-    MenuItem
+const MenuItem = ({ title, price, tags }) => (
+  <div className="app__menuitem">
+    <div className="app__menuitem-head">
+      <div className="app__menuitem-name">
+        <p className="p__cormorant" style={{ color: "#DCCA87" }}>
+          {title}
+        </p>
+      </div>
+
+      {/* this div is for the dash between the titles and price */}
+      <div className="app__menuitem-dash" />
+
+      {/* copy of previous menu {title} div for the price of each menuitem wine */}
+      <div className="app__menuitem-price">
+        <p className="p__cormorant">{price}</p>
+      </div>
+    </div>
+
+    {/* this div is for the tags of each menuitem */}
+    <div className="app__menuitem-sub">
+      <p className="p__opensans" style={{ color: "#AAAAAA" }}>
+        {tags}
+      </p>
+    </div>
   </div>
 );
 
--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,40 @@
-import React from 'react';
-
-import './Chef.css';
+import React from "react";
+import { images } from "../../constants";
+import { SubHeading } from "../../components";
+import "./Chef.css";
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef" />
+    </div>
+
+    {/* this is the div for the content */}
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's Word" />
+      <h1 className="headtext__cormorant">What We Believe In</h1>
+
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote" />
+          <p className="p__opensans">
+            “Once you decide on your occupation, you must immerse yourself in
+            your work.
+          </p>
+        </div>
+        <p className="p__opensans">
+          You have to fall in love with your work. Never complain about your
+          job. You must dedicate your life to mastering your skill. That’s the
+          secret of success and it’s the key to being regarded honorably.” —
+          Jiro Ono
+        </p>
+      </div>
+      <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className="p__opensans">Chef & Co-Founder</p>
+        <img src={images.sign} alt="signature" />
+      </div>
+    </div>
   </div>
 );
 
